2bda8224645e1602db6cbb9674cf10a9808f41be
Wesley Nelson <wesslnelson@aol.com>
2016-06-19 19:36:33 -0400

    Introduced a diceList and a sHole list to puzzleScreen

--- app/src/main/java/com/learnknots/wesslnelson/math_app/screens/PuzzleScreen.java
+++ app/src/main/java/com/learnknots/wesslnelson/math_app/screens/PuzzleScreen.java
@@ -11,8 +11,12 @@ import com.learnknots.wesslnelson.math_app.Draw;
 import com.learnknots.wesslnelson.math_app.R;
 import com.learnknots.wesslnelson.math_app.model.CircleHole;
 import com.learnknots.wesslnelson.math_app.model.Coin;
+import com.learnknots.wesslnelson.math_app.model.DiceManager;
 import com.learnknots.wesslnelson.math_app.model.Die;
 import com.learnknots.wesslnelson.math_app.model.SquareHole;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by wesslnelson on 6/18/16.
@@ -26,14 +30,27 @@ public class PuzzleScreen {
     private Die  firstDie;
     private SquareHole firstSHole;
     private CircleHole firstCHole;
+    private List<Die> diceList;
+    private List<SquareHole> sHoleList;
+    private DiceManager diceManager;
+    private Context context;
+
 
     public PuzzleScreen( Context context) {
+        this.context = context;
+        diceManager = new DiceManager(context);
 
         draw = new Draw();
         firstCoin = new Coin(BitmapFactory.decodeResource(context.getResources(),
                 R.drawable.coin_plus), 300,300, "+");
-        firstDie  = new Die( BitmapFactory.decodeResource(context.getResources(),
-                R.drawable.die_1), 300,100, 1);
+
+
+        diceList = makeDice();
+
+        sHoleList = makeSHoles();
+
+        firstDie = diceManager.getRandomDie();
+        firstDie.setCenter(300, 100);
         firstSHole = new SquareHole( BitmapFactory.decodeResource(context.getResources(),
                 R.drawable.square_hole64), 100, 500);
         firstCHole = new CircleHole( BitmapFactory.decodeResource(context.getResources(),
@@ -43,32 +60,58 @@ public class PuzzleScreen {
     public void render(Canvas canvas) {
         draw.displayTextbyWidth(canvas, "testing", 100, 100, 100);
         // draw holes first, otherwise dice would be under the hole
-        firstSHole.render(canvas);
+        for (SquareHole shole: sHoleList) {
+            shole.render(canvas);
+        }
+        //firstSHole.render(canvas);
         firstCHole.render(canvas);
 
 
         firstCoin.render(canvas);
-        firstDie.render(canvas);
 
-        if (firstSHole.hasMessage()) {
-            draw.displayText(canvas, firstSHole.getContainedMessage(), 100, 200);
+        for (Die die: diceList) {
+            die.render(canvas);
+        }
+
+
+        // just for some looksie  ie not permanent
+        if (sHoleList.get(0).hasMessage()) {
+            draw.displayText(canvas, sHoleList.get(0).getContainedMessage(), 100, 200);
         }
         if (firstCHole.hasMessage()) {
             draw.displayText(canvas, firstCHole.getContainedMessage(), 100, 210);
+        }
+        if (sHoleList.get(1).hasMessage()) {
+            draw.displayText(canvas, sHoleList.get(1).getContainedMessage(), 100, 220);
         }
 
     }
 
     public void update() {
 
+        // sets a hole to empty if necessary
+        for (SquareHole shole: sHoleList) {
+            int counter = 0;
+            for (Die die : diceList) {
+                if (shole.dieOverlaps(die)) {
+                    counter += 1;
+                }
+            }
+            if (counter == 0) {
+                shole.setEmpty(true);
+                shole.setContainedMessage(null);
+            }
+        }
     }
     
 
     public void onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            for (Die die: diceList) {
+                die.handleActionDown((int) event.getX(), (int) event.getY());
+            }
+
             firstCoin.handleActionDown((int) event.getX(), (int) event.getY());
-            firstDie.handleActionDown((int) event.getX(), (int) event.getY());
-            Log.d(TAG, String.valueOf(firstCoin.isTouched()));
         }
         if (event.getAction() == MotionEvent.ACTION_MOVE) {
             if(firstCoin.isTouched()) {
@@ -78,11 +121,10 @@ public class PuzzleScreen {
                     firstCHole.setContainedMessage(null);
                 }
             }
-            if(firstDie.isTouched()) {
-                firstDie.setCenter((int)event.getX(), (int)event.getY());
-                if (!firstSHole.dieOverlaps(firstDie)) {
-                    firstSHole.setEmpty(true);
-                    firstSHole.setContainedMessage(null);
+
+            for (Die die: diceList) {
+                if (die.isTouched()) {
+                    die.setCenter((int)event.getX(), (int)event.getY());
                 }
             }
 
@@ -92,13 +134,37 @@ public class PuzzleScreen {
                 firstCoin.setTouched(false);
                 firstCHole.snapIfClose(firstCoin);
             }
-            if (firstDie.isTouched()) {
-                firstDie.setTouched(false);
-                firstSHole.snapIfClose(firstDie);
+            for (Die die: diceList) {
+                if (die.isTouched()) {
+                    die.setTouched(false);
+                    for (SquareHole shole : sHoleList) {
+                        shole.snapIfClose(die);
+                    }
+                }
             }
 
         }
     }
 
 
+    private List<Die> makeDice() {
+        List<Die> dList = new ArrayList<Die>();
+        dList.add(diceManager.getRandomDie());
+        dList.add(diceManager.getRandomDie());
+        dList.get(0).setCenter(300,150);
+        dList.get(1).setCenter(300,200);
+
+        return dList;
+
+    }
+
+    private List<SquareHole> makeSHoles() {
+        List<SquareHole> shList = new ArrayList<SquareHole>();
+        shList.add(new SquareHole( BitmapFactory.decodeResource(context.getResources(),
+                R.drawable.square_hole64), 100, 500));
+        shList.add(new SquareHole( BitmapFactory.decodeResource(context.getResources(),
+                R.drawable.square_hole64), 300, 500));
+
+        return shList;
+    }
 }
